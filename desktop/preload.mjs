2c596f2db010c070ec0245f52fda94a493b86a41
DarkRajeshow <rajannaadeli@gmail.com>
2025-06-01 16:54:59 +0530

    removed desktop folder

--- desktop/preload.mjs
+++
@@ -1,8 +0,0 @@
-// src/preload.ts or preload.mjs
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    openItemWindow: (urlPath) => ipcRenderer.send('open-item-window', urlPath),
-});
-
-// npx esbuild preload.ts --bundle --platform=node --outfile=build/preload.js
